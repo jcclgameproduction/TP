e4c197458db397a1609d25c16460d8f1db122172
jcclgameproduction <61763550+jcclgameproduction@users.noreply.github.com>
2020-05-26 09:04:21 -0300

    Add files via upload

---
+++ OlaMundo.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : Exemplo.cpp
+// Author      : Aluno/Aluna
+// Version     :
+// Copyright   : This project is under GPL3 license see README file for more information
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "Olá mundo!" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
